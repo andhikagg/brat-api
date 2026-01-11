d2f4d1d34a1f46d6489d69b7f4f5ad5bdfee961e
M Andhika A <andikaafrianto26@gmail.com>
2026-01-11 23:59:23 +0700

    huh?

--- app.js
+++ app.js
@@ -21,86 +21,83 @@ const launchBrowser = async () => {
 
 launchBrowser();
 
-async function fetchCount() {
-  try {
-    return (await axios.get("https://api.counterapi.dev/v1/aqul/brat/up")).data?.count || 0
-  } catch {
-    return 0
-  }
-}
-
-app.use('*', async (req, res) => {
-  const text = req.query.text
-  const background = req.query.background
-  const color = req.query.color
-  const hit = fetchCount()
+app.use('/gen', async (req, res) => {
+  const { text, background, color } = req.query;
+  
   if (!text) return res.status(200).json({
-    author: 'zennn08 (aqul)',
-    repository: {
-      github: 'https://github.com/zennn08/brat-api/'
-    },
-    hit: await hit,
-    message: "Parameter `text` diperlukan",
-    runtime: {
-      os: os.type(),
-      platform: os.platform(),
-      architecture: os.arch(),
-      cpuCount: os.cpus().length,
-      uptime: `${os.uptime()} seconds`,
-      memoryUsage: `${Math.round((os.totalmem() - os.freemem()) / 1024 / 1024)} MB used of ${Math.round(os.totalmem() / 1024 / 1024)} MB`
-    }
+    status: false,
+    code: "required",
+    message: "Parameter `text` is required!",
   })
-  if (!browser) {
-    await launchBrowser();
+  
+  try {
+    if (!browser) {
+      await launchBrowser();
+    }
+    
+    const context = await browser.newContext({
+      viewport: {
+        width: 1536,
+        height: 695
+      }
+    });
+    const page = await context.newPage();
+    
+    const filePath = path.join(__dirname, './site/index.html');
+    
+    // Open https://www.bratgenerator.com/
+    await page.goto(`file://${filePath}`);
+    
+    // Click on <div> #toggleButtonWhite
+    await page.click('#toggleButtonWhite');
+    
+    // Click on <div> #textOverlay
+    await page.click('#textOverlay');
+    
+    // Click on <input> #textInput
+    await page.click('#textInput');
+    
+    // Fill "sas" on <input> #textInput
+    await page.fill('#textInput', text);
+    
+    await page.evaluate((data) => {
+      if (data.background) {
+        $('.node__content.clearfix').css('background-color', data.background);
+      }
+      if (data.color) {
+        $('.textFitted').css('color', data.color);
+      }
+    }, { background, color });
+    
+    const element = await page.$('#textOverlay');
+    const box = await element.boundingBox();
+    
+    res.set('Content-Type', 'image/png');
+    res.end(await page.screenshot({
+      clip: {
+        x: box.x,
+        y: box.y,
+        width: 500,
+       height: 500
+      }
+    }));
+    
+  } catch(e) {
+    res.code(500).send({
+      status: false,
+      code: "error",
+      message: e.message
+    })
+  } finally {
+    await context.close();
   }
-  const context = await browser.newContext({
-    viewport: {
-      width: 1536,
-      height: 695
-    }
-  });
-  const page = await context.newPage();
-
-  const filePath = path.join(__dirname, './site/index.html');
-
-  // Open https://www.bratgenerator.com/
-  await page.goto(`file://${filePath}`);
-
-  // Click on <div> #toggleButtonWhite
-  await page.click('#toggleButtonWhite');
-
-  // Click on <div> #textOverlay
-  await page.click('#textOverlay');
-
-  // Click on <input> #textInput
-  await page.click('#textInput');
-
-  // Fill "sas" on <input> #textInput
-  await page.fill('#textInput', text);
-
-  await page.evaluate((data) => {
-    if (data.background) {
-      $('.node__content.clearfix').css('background-color', data.background);
-    }
-    if (data.color) {
-      $('.textFitted').css('color', data.color);
-    }
-  }, { background, color });
-
-  const element = await page.$('#textOverlay');
-  const box = await element.boundingBox();
-
-  res.set('Content-Type', 'image/png');
-  res.end(await page.screenshot({
-    clip: {
-      x: box.x,
-      y: box.y,
-      width: 500,
-      height: 500
-    }
-  }));
-  await context.close();
 });
+
+app.get("/", (req, res) => {
+  res.send({
+    msg: "Hello World"
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
